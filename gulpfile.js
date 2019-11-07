59a81b8148f861318742e850526165128167e66c
weniver <weniver@wenivers-MacBook-Pro.local>
2019-11-07 16:14:13 -0600

    Redesign of all page

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,59 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pkg = require("./package.json");
+var sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
 
 // Copy third party libraries from /node_modules into /assets
-gulp.task('assets', function() {
-
+gulp.task("assets", function() {
   // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  gulp
+    .src([
+      "./node_modules/bootstrap/dist/**/*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
     ])
-    .pipe(gulp.dest('./assets/bootstrap'))
+    .pipe(gulp.dest("./assets/bootstrap"));
 
   // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+  gulp
+    .src([
+      "./node_modules/jquery/dist/*",
+      "!./node_modules/jquery/dist/core.js"
     ])
-    .pipe(gulp.dest('./assets/jquery'))
+    .pipe(gulp.dest("./assets/jquery"));
 
   // jQuery Easing
-  gulp.src([
-      'node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('assets/jquery-easing'))
+  gulp
+    .src(["node_modules/jquery.easing/*.js"])
+    .pipe(gulp.dest("assets/jquery-easing"));
+});
 
-})
+//sass to css
+gulp.task("sass", function() {
+  return gulp
+    .src("./css/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
 
 // Default task
-gulp.task('default', ['assets']);
+gulp.task("default", ["assets"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function() {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
+  gulp.watch("./css/*.scss", ["sass"]);
+  gulp.watch("./css/*.css").on("change", browserSync.reload);
+  gulp.watch("./css/*.scss").on("change", browserSync.reload);
+  gulp.watch("./js/*.js").on("change", browserSync.reload);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 });
 
 // Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./js/*.js', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task("dev", ["browserSync"]);
--- js/alumnica.js
+++ js/alumnica.js
@@ -1,57 +1,87 @@
-(function($) {
-  "use strict"; // Start of use strict
+
+
+$(function() {
+  // Start of use strict
+  "use strict";
+
+  $("#navbar").load("nav_bar.htm");
+  $("#footer").load("footer.html");
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 54
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
+  $("body").scrollspy({
+    target: "#mainNav",
     offset: 54
   });
+
   //Hamburger toggle
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
     $hamburger.toggleClass("is-active");
   });
-})(jQuery); // End of use strict
+
+  //Input's Label Interaction
+  var input = $('input');
+  var textarea = $('textarea')
+
+  input.focus(function(){
+    $(this).parents('.form-group').addClass('focused');
+  });
+
+  input.blur(function(){
+    var inputValue = $(this).val();
+    if ( inputValue == "" ) {
+      $(this).parents('.form-group').removeClass('focused');
+    }
+  })
+
+  textarea.focus(function(){
+    $(this).parents('.form-group').addClass('focused');
+  });
+
+  textarea.blur(function(){
+    var inputValue = $(this).val();
+    if ( inputValue == "" ) {
+      $(this).parents('.form-group').removeClass('focused');
+    }
+  })
 
 
- 
-
-
-function sendMail(){  
- emailjs.sendForm('gmail', 'template_sJc33IhK', '#contact-form')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-
-    //console.log ("Gracias por escribirnos")
-
-}
-
- 
-        
-$(function(){
-  $("#navbar").load("nav_bar.htm"); 
-  $("#footer").load("footer.htm"); 
+  function sendMail() {
+    emailjs.sendForm("gmail", "template_sJc33IhK", "#contact-form").then(
+      function(response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function(error) {
+        console.log("FAILED...", error);
+      }
+    );
+  }
+  // End of use strict
 });
-
