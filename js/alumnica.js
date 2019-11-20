d827c587c394454288fc6fca1bdc795ccad2db8e
weniver <weniver@wenivers-MacBook-Pro.local>
2019-11-20 11:57:04 -0600

    Add basic submit feedback

--- js/alumnica.js
+++ js/alumnica.js
@@ -3,16 +3,17 @@
     // Start of use strict
     "use strict";
 
-    $("#navbar").load("nav_bar.html", function(){
+    $("#footer").load("footer.html");
+
+    $("#navbar").load("nav_bar.html", function() {
       var hamburger = $(".hamburger");
 
       hamburger.click(function() {
-        $('#mobileMenu').collapse('toggle')
-        $(this).toggleClass("is-active")
+        $("#mobileMenu").collapse("toggle");
+        $(this).toggleClass("is-active");
       });
-    })
+    });
 
-    $("#footer").load("footer.html");
 
     //Input's Label Interaction
     var input = $("input");
@@ -48,50 +49,56 @@
       }
     });
 
-    function sendMail() {
-      emailjs.sendForm("gmail", "template_sJc33IhK", "#contact-form").then(
-        function(response) {
-          console.log("SUCCESS!", response.status, response.text);
-        },
-        function(error) {
-          console.log("FAILED...", error);
-        }
+    //contact_form
+    $("#contact_button").click(function send_contact() {
+      console.log("in contact");
+      let name = $("#nombre").val();
+      let email = $("#email").val();
+      let phone = $("#telefono").val();
+      let msg = $("#mensaje").val();
+
+      fetch("http://127.0.0.1:8000/contacts/contact/", {
+        method: "post",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }),
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          phone: phone,
+          msg: msg
+        })
+      });
+
+      $("#success_contact").toggleClass("d-none");
+      $("<h3>¡Muchas gracias por tu interés " + name + "!</h3>").prependTo(
+        "#success_contact"
       );
-    }
+      $("#contact_form").toggleClass("d-none");
+    });
+
+    //subscriber_form
+    $("#subscriber_button").click(function send_subscriber() {
+      console.log("in suscribe");
+
+      let email = $("#email_suscriber").val()
+
+      fetch("http://127.0.0.1:8000/contacts/suscriber/", {
+        method: "post",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }),
+        body: JSON.stringify({
+          email: email
+        })
+      });
+
+      $("#success_subscriber").toggleClass("d-none");
+      $("#subscribe_form").toggleClass("d-none");
+    });
+
     // End of use strict
   });
 })(jQuery, window, document);
-
-function send_suscriber(){
-  console.log("in suscribe")
-
-  fetch('http://127.0.0.1:8000/contacts/suscriber/', {
-  	method: 'post',
-    headers: new Headers({
-		'Content-Type': 'application/json',
-    'Accept': 'application/json'
-    }),
-  	body: JSON.stringify({
-  		email: document.getElementById('email_suscriber').value,
-  	})
-  });
-}
-
-function send_contact(){
-  console.log("in contact")
-
-  fetch('http://127.0.0.1:8000/contacts/contact/', {
-  	method: 'post',
-    headers: new Headers({
-		'Content-Type': 'application/json',
-    'Accept': 'application/json'
-    }),
-  	body: JSON.stringify({
-      name: document.getElementById('nombre').value,
-  		email: document.getElementById('email').value,
-      phone: document.getElementById('telefono').value,
-      msg: document.getElementById('mensaje').value,
-
-  	})
-  });
-}
