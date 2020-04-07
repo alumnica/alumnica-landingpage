e697eff187a1219f3c2e97bc152de10d5dfe6408
weniver <weniver@gmail.com>
2020-04-07 12:13:12 -0500

    Removes js file that was there by mistake

--- Untitled.js
+++
@@ -1,9 +0,0 @@
-function addDatesToCalendar(fechas) {
-  let array = [];
-  fechas.forEach(function(row) {
-    row.forEach(function(fecha) {
-      array.push(fecha);
-    });
-  });
-  return array;
-}
